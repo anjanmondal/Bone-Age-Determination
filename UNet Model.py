ad3d7781a2f9f9ba757c1cf63fa928c17c454738
Anjan Mondal <95505124+anjanmondal@users.noreply.github.com>
2022-03-14 17:38:02 +0530

    Update UNet Model.py
    Added Up-sampling block to model.

--- UNet Model.py
+++ UNet Model.py
@@ -7,16 +7,12 @@ Created on Sun Mar 13 21:22:26 2022
 
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Activation, ReLU
+from tensorflow.keras.layers import Conv2DTranspose, Concatenate, Model
 
-inputs = tf.keras.Input(shape=(3,4,4))
-x = tf.keras.layers.Dense(4, activation=tf.nn.relu)(inputs)
-outputs = tf.keras.layers.Dense(5, activation=tf.nn.softmax)(x)
-model = tf.keras.Model(inputs=inputs, outputs=outputs)
-    
 
-def conv_block(image_input, filters,max_pooling=True):
+def conv_block(tensor_input, filters,max_pooling=True):
     conv= Conv2D(filters=filters,padding='valid',
-    kernel_size=(3,3),kernel_initializer='he_normal',activation='relu')(image_input)
+    kernel_size=(3,3),kernel_initializer='he_normal',activation='relu')(tensor_input)
     
     conv=Conv2D(filters=filters,padding='valid',
     kernel_size=(3,3),kernel_initializer='he_normal',activation='relu')(conv)
@@ -31,19 +27,40 @@ def conv_block(image_input, filters,max_pooling=True):
     
     return next_layer,skip_connection
 
-
-
-
-def unet_model(input_size,n_filters,n_classes):
+def upsampling_block(prev_input, skip_input, n_filters=64):
+    up=Conv2DTranspose(n_filters,kernel_size=(2,2),strides=(2,2))
+    
+    merge = Concatenate([up, skip_input], axis=3)(prev_input)
+    
+    conv=Conv2D(filters=n_filters, kernel_size=(3,3), activation='relu',
+                kernel_initializer='he_normal')(merge)
+    
+    conv=Conv2D(filters=n_filters, kernel_size=(3,3), activation='relu',
+                kernel_initializer='he_normal')(conv)
+    
+    
+    return conv
+    
+def unet_model(input_size,n_classes,n_filters=64):
+    inputs = Input(input_size)
     cblock1 = conv_block(inputs,n_filters)
-    # Chain the first element of the output of each block to be the input of the next conv_block. 
-    # Double the number of filters at each new step
     cblock2 = conv_block(cblock1[0], 2*n_filters)
     cblock3 = conv_block(cblock2[0], 4*n_filters)
     cblock4 = conv_block(cblock3[0], 8*n_filters)
     cblock5 = conv_block(cblock4[0], 16*n_filters, max_pooling=False)
     
     
-    
-    
-    
+    ublock6 = upsampling_block(cblock5[0], cblock4[1],  n_filters*8)
+    ublock7 = upsampling_block(ublock6, cblock3[1],  n_filters*4)
+    ublock8 = upsampling_block(ublock7, cblock2[1],  n_filters*2)
+    ublock9 = upsampling_block(ublock8, cblock1[1],  n_filters)
+
+
+    conv9 = Conv2D(n_filters,
+                 kernel_size=(3,3),
+                 activation='relu',
+                 kernel_initializer='he_normal')(ublock9)
+
+    #conv10 = Conv2D(filters=n_classes, kernel_size=(1,1))(conv9)    
+    model = Model(inputs=inputs, outputs=conv9)
+    return model
